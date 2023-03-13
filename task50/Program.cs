781d21b4f0b2e65d71a48309d01c2493e1e69f3f
Svetlana <sypkovas@mail.ru>
2023-03-13 20:59:39 +0300

    решение task52

---
+++ task50/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве, 
+// и возвращает позицию этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],2} \t");
+        Console.WriteLine();
+    }
+}
+void FindNumber(int[,] array1)
+{
+    Console.WriteLine("Введите искомое значение");
+    int search = Convert.ToInt32(Console.ReadLine());
+    int[] result = new int[2];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            if (array1[i, j] == search) 
+            {
+                result[0] = i;
+                result[1] = j;
+                break;
+            }
+        }
+        if (result[0] !=0) break;
+    }
+    if (result[0] !=0) Console.WriteLine($"Позиция этого элемента: [{result[0]},{result[1]}]");
+    else Console.WriteLine($"-> такого числа в массиве нет");
+}
+
+FillArray(array);
+PrintArray(array);
+FindNumber(array);
