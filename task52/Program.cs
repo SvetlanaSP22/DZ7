6dc3c719fc808e79cc4633954ec15c5ccf3b993b
Svetlana <sypkovas@mail.ru>
2023-03-13 21:31:22 +0300

    решение task52

---
+++ task52/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],2} \t");
+        Console.WriteLine();
+    }
+}
+void FindSrArifm(int[,] array1)
+{
+    double[] srarifm = new double [array1.GetLength(1)];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+            srarifm[j] = srarifm[j] + array1[i,j];
+    }
+    for (int i = 0; i < array1.GetLength(1); i++)
+    {
+        srarifm[i] = Math.Round(srarifm[i] / array1.GetLength(0), 1);
+        Console.WriteLine($"Среднее арифметическое столбца {i} = {srarifm[i]}");
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+FindSrArifm(array);
+
